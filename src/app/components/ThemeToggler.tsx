9fe06361ae530b97f74867b2e6d366a618ffc559
danielbuva <daniel.valdecantos@gmail.com>
2024-09-19 14:33:54 -0700

    style: move theme toggler to the right

--- src/app/components/ThemeToggler.tsx
+++ src/app/components/ThemeToggler.tsx
@@ -7,7 +7,7 @@ export default function ThemeToggler() {
   const { toggleMode, theme } = useTheme();
 
   return (
-    <button onClick={toggleMode}>
+    <button onClick={toggleMode} className="absolute right-0 px-1 pt-[2px]">
       <LightDarkSVG dark={theme === "dark"} />
     </button>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,26 @@ import ThemeToggler from "./components/ThemeToggler";
 export default function HomePage() {
   return (
     <main className="bg-gradient flex min-h-screen flex-col items-center border border-red-500">
-      <div className="container flex h-10 items-start justify-center gap-2 py-8 text-sm text-[#c7dde375]">
-        <Link id="nav" href="/" className="h-8 px-3 hover:text-[#c7dde3]">
-          Home
-        </Link>
-        <Link
-          id="nav"
-          href="/projects"
-          className="h-8 px-3 hover:text-[#c7dde3]"
-        >
-          Projects
-        </Link>
-        <Link id="nav" href="/resume" className="h-8 px-3 hover:text-[#c7dde3]">
-          Resume
-        </Link>
+      <div className="container relative flex h-10 w-[600px] justify-center py-8 text-sm text-[#c7dde375]">
+        <div>
+          <Link id="nav" href="/" className="h-8 px-3 hover:text-[#c7dde3]">
+            Home
+          </Link>
+          <Link
+            id="nav"
+            href="/projects"
+            className="h-8 px-3 hover:text-[#c7dde3]"
+          >
+            Projects
+          </Link>
+          <Link
+            id="nav"
+            href="/resume"
+            className="h-8 px-3 hover:text-[#c7dde3]"
+          >
+            Resume
+          </Link>
+        </div>
         <ThemeToggler />
       </div>
       <div className="w-[598px] flex-grow rounded-3xl border">hi</div>
