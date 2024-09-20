19a1fd89e2d25aa9556db679587aa63f3eea28fe
danielbuva <daniel.valdecantos@gmail.com>
2024-09-20 16:35:43 -0700

    feat: add pdf link on resume page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,10 +1,22 @@
+import Link from "next/link";
 import EducationEntries from "./components/EducationEntries";
 import WorkExperienceEntries from "./components/WorkExperienceEntries";
 
 export default function Resume() {
   return (
     <div>
-      <div className="pb-4 text-xl font-bold">Work Experience</div>
+      <div className="flex items-center justify-between pb-4">
+        <p className="text-xl font-bold">Work Experience</p>
+        <Link
+          className="text-sm"
+          id="link"
+          href="/daniresume.pdf"
+          target="_blank"
+          prefetch
+        >
+          PDF ↗
+        </Link>
+      </div>
       <WorkExperienceEntries />
       <div className="py-4 text-xl font-bold">Education</div>
       <EducationEntries />
