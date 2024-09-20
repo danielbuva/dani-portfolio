c904ed554e79b6cd9251c941eb806d892c96448a
danielbuva <daniel.valdecantos@gmail.com>
2024-09-20 16:58:04 -0700

    feat: add titles of my projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,11 @@
 export default function Projects() {
-  return <div>projects!</div>;
+  return (
+    <div>
+      <p>millio one</p>
+      <p>goaly</p>
+      <p>YCHSHIP</p>
+      <p>musy</p>
+      <p>vs code themes</p>
+    </div>
+  );
 }
