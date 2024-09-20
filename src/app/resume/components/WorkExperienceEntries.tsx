711527a63fac75c9fdc2468821f5a7e80c3fdd75
danielbuva <daniel.valdecantos@gmail.com>
2024-09-20 16:32:43 -0700

    fix: work entry technology

--- src/app/resume/components/WorkExperienceEntries.tsx
+++ src/app/resume/components/WorkExperienceEntries.tsx
@@ -35,7 +35,7 @@ export default function WorkExperienceEntries() {
           "Typescript",
           "Tailwind",
           "Figma",
-          "Shopify",
+          "Framer Motion",
         ]}
       />
     </>
